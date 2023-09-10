6ed66cf902fa914cfd06caa2ebe7ce08aae21e2d
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2023-09-10 20:21:23 +0530

    Calculate multi&&add & display results

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
 let firstNumArr = [];
 let firstNumber;
 
-let secondNumArr = [];
-let secondNumber;
+let currentNumArr = [];
+let currentNumber;
 
+let toggle = true;
 
-let operator;
-let toggle = 'active';
+let result;
 
-const getNumbers = document.querySelectorAll('.num');
-
-
-getNumbers.forEach((num)=> num.addEventListener('click',()=>{
-    if (operator == 'multi' || operator == 'add' && !toggle) {
-        secondNumArr.push(num.textContent);
-        secondNumber = parseFloat(secondNumArr.join(''));
-        getDisplay.textContent = secondNumber
-        console.log(`second_InGetNum : ${secondNumber}`)
-    }
-    if(toggle == 'active'){
-        console.log(`${num.textContent}`)
-        firstNumArr.push(num.textContent);
-        firstNumber = parseFloat(firstNumArr.join(''))
-        getDisplay.textContent = firstNumber
-        console.log(`first_inGetNum: ${firstNumber}`)
-    }
-}))
-
-// let operationsObj ={
-//     addEquals : function (firstNumber , secondNumber){
-//         return firstNumber + secondNumber
-//     }
-// }
 const getDisplay = document.querySelector('.display > .current')
 
+const operationBtns = document.querySelectorAll('.operator');
+
+let operator;
 function display(string , bool){
     if(bool){
     getDisplay.textContent += string;
@@ -43,58 +22,77 @@ function display(string , bool){
     }
 }
 
-
-const addBtn = document.querySelector('#add')
-const equalBtn = document.querySelector('#equal');
-
-addBtn.addEventListener('click',(e)=>{
-    operator = addBtn.id
-    toggle = false;
-    console.log(`first_inAddBtn: ${firstNumber}`)
-    console.log(`second_inADdBTn : ${secondNumber}`)
-    operation(operator , e)
-    e.target.classList.add('active')
-})
-
-const multiBtn = document.querySelector('#multi');
-
-multiBtn.addEventListener('click',(e)=>{
-    operator = multiBtn.id
-    toggle = false;
-    operation(operator , e)
-    e.target.classList.add('active')
-    console.log(`first_inMultiBtn: ${firstNumber}`)
-    console.log(`second_inMultiBTn : ${secondNumber}`)
-})
+const getNumbers = document.querySelectorAll('.num');
 
 
-function operation(operator , e){
-    if ( operator == "add"){
-        display(`${firstNumber}+`)
-    if (operator == 'add' && e.target.classList.contains('active')){
-        result = parseFloat(firstNumber) + parseFloat(secondNumber);
-            console.log(`result : ${result}`)
-            display(`${result}+`, false)
-            firstNumber = result;
-            console.log(`first_afterAddBtn: ${firstNumber}`)
-            secondNumArr = [];
-            secondNumber = 0;
-            console.log(`second_afterADdBTn : ${secondNumber}`)
+getNumbers.forEach((num)=> num.addEventListener('click',()=>{
+    if (!toggle){
+
+            currentNumArr.push(num.textContent);
+            currentNumber = parseFloat(currentNumArr.join(''));
+            display(`${currentNumber}`);
+            console.log(`current_InGetNum : ${currentNumber}`)
         }
+    if(toggle){
+        console.log(`${num.textContent}`)
+        firstNumArr.push(num.textContent);
+        firstNumber = parseFloat(firstNumArr.join(''))
+        display(`${firstNumber}`);
+        console.log(`first_inGetNum: ${firstNumber}`)
     }
-    if ( operator == "multi"){
-        display(`${firstNumber}x`)
-    }
-    if (operator == 'multi' && e.target.classList.contains('active')){
-        result = ((parseFloat(firstNumber))*(parseFloat(secondNumber)));
-            console.log(`result : ${result}`)
-            display(`${result}x`, false)
-            firstNumber = result;
-            console.log(`first_afterMutliBtn: ${firstNumber}`)
-            secondNumArr = [];
-            secondNumber = 0;
-            console.log(`second_afterMutliBTn : ${secondNumber}`)
-    }
+}))
+
+
+function activeOperator (e , operationBtns){
+    operationBtns.forEach((button)=> (button.classList.remove('active')));
+    e.target.classList.add('active');
+    console.log(`ACTIVATED : ${e.target.id} ${e.target.classList.value}`)
 }
 
+operationBtns.forEach(
+    (operatorBtn)=> operatorBtn.addEventListener(('click'), 
+        (e)=>{
+            display(`${firstNumber}${e.target.textContent}`)
+            toggle = false;
+            console.log(`first_inOperatorBtn: ${firstNumber}`)
+            console.log(`current_inOperatorBTn : ${currentNumber}`)
+            operation(operator , e);
+            operator = e.target.id
+            console.log(`${ e.target.id}`)
+            activeOperator(e , operationBtns);
+            console.log('operator is 2nd')
+        }
+    )
+)
+             
+function operation(operator , e){
+    console.log('func is first ')
+    if (!currentNumber && currentNumber !== 0)  return;
+    console.log(`GOT IT : ${e.target.id} ${e.target.classList.value}`)
+    if (operator == 'add'){
+        result = parseFloat(firstNumber) + parseFloat(currentNumber);
+        firstNumber = result;
+        console.log(`result : ${result}`)
+        console.log(`first_afterAddBtn: ${firstNumber}`)
+        console.log(`current_afterADdBTn : ${currentNumber}`)
+    }
+    if (operator == 'multi'){
+        result = ((parseFloat(firstNumber))*(parseFloat(currentNumber)));
+        console.log(`result : ${result}`)
+        // display(`${result}x`, false)
+        firstNumber = result;
+        console.log(`first_afterMutliBtn: ${firstNumber}`)
+        console.log(`current_afterMutliBTn : ${currentNumber}`)
+    }
+    currentNumArr = [];
+    currentNumber = null;
+}
 
+operationBtns.forEach(
+    (operatorBtn)=> operatorBtn.addEventListener(('click'), 
+        (e)=>{
+            if (!result){
+                display(`${firstNumber}${e.target.textContent}`, false)
+            }
+            else {display(`${result}${e.target.textContent}`, false)}
+        }))

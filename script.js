fda6e099b4591bbf33ca06b8cf8d5012a5f1f5f5
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2023-09-11 20:28:10 +0530

    Remove consoleLogs && add DisplayLogs

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let firstNumArr = [];
-let firstNumber;
-
-let currentNumArr = [];
-let currentNumber;
-
-let toggle = true;
-
-let result;
-
-
-
 const getDisplayLog = document.querySelector('.display > .log')
 const getDisplayCurr = document.querySelector('.display > .current')
-
 const operationBtns = document.querySelectorAll('.operator');
+const dotButton = document.querySelector('#dot')
+const getNumbers = document.querySelectorAll('.num');
+const getACBtn = document.querySelector('#ac');
+const getCBtn = document.querySelector('#del')
 
+let firstNumArr = [];
+let firstNumber;
+let currentNumArr = [];
+let currentNumber;
+let toggle = true;
+let result;
 let operator;
+
+displayCurr('0')
+
 function displayLog(string , bool){
     if(bool){
         getDisplayLog.textContent += string;
@@ -25,102 +25,101 @@ function displayLog(string , bool){
     }
 }
 
-// function displayCurrent (string , bool){};
-
-const getNumbers = document.querySelectorAll('.num');
-
-
-getNumbers.forEach((num)=> num.addEventListener('click',()=>{
-    if (!toggle){
-       {
-            currentNumArr.push(num.textContent);
-            currentNumber = parseFloat(currentNumArr.join(''));
-            displayLog(`${currentNumber}`);
-            console.log(`current_InGetNum : ${currentNumber}`)
-        }
-        }
-    if(toggle){
-        if (num.id ==  'dot' && block)onceDeciPoint(num , arr);
-        else {
-        console.log(`${num.textContent}`)
-        firstNumArr.push(num.textContent);
-        firstNumber = parseFloat(firstNumArr.join(''))
-        displayLog(`${firstNumber}`);
-        console.log(`first_inGetNum: ${firstNumber}`)
-        }
+function displayCurr (string , bool){
+    if(bool){
+        getDisplayCurr.textContent += string;
     }
-}))
+    else {
+        getDisplayCurr.textContent = string;
+    }
+};
 
 
 function activeOperator (e , operationBtns){
     operationBtns.forEach((button)=> (button.classList.remove('active')));
     e.target.classList.add('active');
-    console.log(`ACTIVATED : ${e.target.id} ${e.target.classList.value}`)
 }
 
-operationBtns.forEach(
-    (operatorBtn)=> operatorBtn.addEventListener(('click'), 
-    (e)=>{
-        if (operatorBtn.id == 'ac')return;
-        displayLog(`${firstNumber}${e.target.textContent}`)
-        toggle = false;
-        console.log(`first_inOperatorBtn: ${firstNumber}`)
-            console.log(`current_inOperatorBTn : ${currentNumber}`)
-            operation(operator , e);
-            operator = e.target.id
-            console.log(`${ e.target.id}`)
-            activeOperator(e , operationBtns);
-            console.log('operator is 2nd')
+function clear(arr , num ){
+    arr.pop();
+    num =parseFloat( arr.join(''))
+    console.log('popping')
+}
+
+dotButton.onclick = () => (dotButton.disabled = true)
+
+getNumbers.forEach((num)=> num.addEventListener('click',()=>{
+    if (!toggle){
+        currentNumArr.push(num.textContent);
+            currentNumber = parseFloat(currentNumArr.join(''));
+            displayCurr(`${currentNumber}`);
         }
-    )
-)
-             
-function operation(operator , e){
-    console.log('func is first ')
+        if(toggle){
+            firstNumArr.push(num.textContent);
+            firstNumber = parseFloat(firstNumArr.join(''))
+            displayCurr(`${firstNumber}`);
+        }
+    }))
+    
+function operation(operator){
     if (!currentNumber && currentNumber !== 0)  return;
-    console.log(`GOT IT : ${e.target.id} ${e.target.classList.value}`)
     if (operator == 'add'){
         result = parseFloat(firstNumber) + parseFloat(currentNumber);
-        firstNumber = result;
-        console.log(`result : ${result}`)
-        console.log(`first_afterAddBtn: ${firstNumber}`)
-        console.log(`current_afterADdBTn : ${currentNumber}`)
     }
     if (operator == 'multi'){
         result = ((parseFloat(firstNumber))*(parseFloat(currentNumber)));
-        firstNumber = result;
-        console.log(`result : ${result}`)
-        console.log(`first_afterMutliBtn: ${firstNumber}`)
-        console.log(`current_afterMutliBTn : ${currentNumber}`)
     }
     if (operator == 'sub'){
         result = ((parseFloat(firstNumber))-(parseFloat(currentNumber)));
-        firstNumber = result;
     }
     if (operator == 'divide'){
         {result = ((parseFloat(firstNumber))/(parseFloat(currentNumber)));
-        firstNumber = result;
     }
     }
     if (operator == 'exp'){
         result = ((parseFloat(firstNumber))**(parseFloat(currentNumber)));
-        firstNumber = result;
     }
+    firstNumber = result;
     currentNumArr = [];
     currentNumber = null;
 }
+    
+    
+operationBtns.forEach(
+    (operatorBtn)=> operatorBtn.addEventListener(('click'), 
+    (e)=>{
+
+    if (operatorBtn.id == 'ac')return;
+    if (operatorBtn.id == 'del')return;
+    if (currentNumber){
+        displayLog(`${currentNumber}${e.target.textContent}`, true);
+    }
+    if(firstNumber && !currentNumber){
+            displayLog(`${firstNumber}${e.target.textContent}`);
+        }
+        
+        toggle = false;
+        operation(operator);
+        operator = e.target.id
+        activeOperator(e , operationBtns);
+    }
+)
+)
+             
 
 operationBtns.forEach(
     (operatorBtn)=> operatorBtn.addEventListener(('click'), 
         (e)=>{
             if (operatorBtn.id == 'ac')return;
-        if (!result){
+            if (operatorBtn.id == 'del')return;
+        if (!result ){
             if (operator == 'exp'){
                 displayLog(`${firstNumber}^`, false)
             }
             else {
                 if ( operator == 'equal'){
-                    displayLog('Did You Know.. ')
+                    displayLog("Hmmmmmm...")
+                    displayCurr(`Interesting..!`)
                 }
                 else{
                 displayLog(`${firstNumber}${e.target.textContent}`, false)
@@ -132,23 +131,21 @@ operationBtns.forEach(
                 displayLog(`${result}^`, false)
             }
             else{
+                if ( !result ){
+                    displayLog('* _ *')
+                }
                     if ( operator == 'equal' && result){
-                        displayLog(`${result}`)
-                        if ( !result ){
-                            displayLog('* _ *')
-                        }
+                        displayCurr(`${result}`)
                     }
                     else{
-                        displayLog(`${result}${e.target.textContent}`, false)
+                        displayCurr(`${result}`)
                     }
                 }
             }
         }
-        ))
+)   )
         
         
-        
-const getACBtn = document.querySelector('#ac');
 
 getACBtn.onclick = () => {
     firstNumArr = [];
@@ -158,4 +155,5 @@ getACBtn.onclick = () => {
     toggle = true;
     result = null;
     displayLog('')
+    displayCurr('0')
 }
